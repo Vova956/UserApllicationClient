d0a30cbf7a21e2e05046641db3eb02edf482bdf3
Vova956 <60294372+Vova956@users.noreply.github.com>
2022-06-28 11:08:48 +0300

    Final Commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,31 @@
-import logo from './logo.svg';
-import './App.css';
+import { TabContext, TabList, TabPanel } from "@mui/lab"
+import { Box, Tab } from "@mui/material"
+import * as React from 'react';
+
+import AddingForm from "./AddingForm";
+import ViewingForm from "./ViewingForm";
+import GetUserID from "./getUserID"
 
 function App() {
+  const [value, setValue] = React.useState('1');
+
+  const handleChange = (event, newValue) => {
+    setValue(newValue);
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <TabContext value={value}>
+      <Box sx={{ borderBottom: 1, borderColor: 'divider' }}>
+        <TabList onChange={handleChange} aria-label="lab API tabs example">
+          <Tab label="Add user" value="1" />
+          <Tab label="View users" value="2" />
+          <Tab label="Get user by ID" value="3" />
+        </TabList>
+      </Box>
+      <TabPanel value="1"><AddingForm/></TabPanel>
+      <TabPanel value="2"><ViewingForm/></TabPanel>
+      <TabPanel value="3"><GetUserID/></TabPanel>
+    </TabContext>
   );
 }
 
